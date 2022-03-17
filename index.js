c082b81acea0273ebc99ba7b450989e8a0b0e6f8
Daniela10289 <dpalacio2200@gmail.com>
2022-03-17 12:42:32 -0500

    Se realiza configuración inical del proyecto

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+
+
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/errorHandler');
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
 })
+
+app.use(logErrors);
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
 
 app.listen(port, () => {
   console.log('Mi port: ' + port);
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,39 @@
+const { ValidationError } = require('sequelize');
+// const boom = require('@hapi/boom');
+
+function logErrors (err, req, res, next) {
+    console.log('logErrors');
+    console.error(err);
+    next(err); // si se le envia el error es un middlewares de error
+}
+
+// un middlewares de tipo error debe tener los 4 parametros
+function errorHandler(err, req, res, next) {
+    console.log('errorHandler');
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+        // si no tiene next terminaria el proceso
+    });
+}
+
+function boomErrorHandler(err, req, res, next) {
+    if (err.isBoom) {
+        const { output } = err;
+        res.status(output.statusCode).json(output.payload);
+    }
+    next(err);
+}
+
+function ormErrorHandler(err, req, res, next) {
+    if (err instanceof ValidationError) {
+      res.status(409).json({
+        statusCode: 409,
+        message: err.name,
+        errors: err.errors
+      });
+    }
+    next(err);
+  }
+
+module.exports = { logErrors, errorHandler, boomErrorHandler, ormErrorHandler }
